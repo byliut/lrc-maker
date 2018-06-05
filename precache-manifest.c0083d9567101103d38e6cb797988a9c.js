073f6c7fdb2df4e4565dad9d6414a5a71003df22
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-05 01:45:43 +0000

    Deploy hufan-akari/lrc-maker to github.com/hufan-akari/lrc-maker.git:gh-pages

---
+++ precache-manifest.c0083d9567101103d38e6cb797988a9c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b7e9bc59a12534de6a5e2e4f0788b8b",
+    "url": "favicons/favicon.ico"
+  },
+  {
+    "revision": "d0fe8bf1df2cf7544772f6d55c8fa31a",
+    "url": "favicons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "da18af5963cfad4f948e33a1096528cf",
+    "url": "favicons/browserconfig.xml"
+  },
+  {
+    "revision": "d3c7c656066f4586c46c1328def6a94b",
+    "url": "favicons/favicon-16x16.png"
+  },
+  {
+    "revision": "c59650a7227145050afb4f281a18585e",
+    "url": "favicons/favicon-32x32.png"
+  },
+  {
+    "revision": "8e5dfc86f15b381a67d9ddcdd9f99fff",
+    "url": "favicons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "3bba68a52c08b7d48d40073b21711cd9",
+    "url": "favicons/mstile-310x310.png"
+  },
+  {
+    "revision": "445ee5adc9f96a5e9095180451b1fcef",
+    "url": "favicons/mstile-150x150.png"
+  },
+  {
+    "revision": "6151c0a58084dd66dca2002325664e74",
+    "url": "favicons/apple-touch-icon.png"
+  },
+  {
+    "revision": "93064af80c087f2b3ad355594851fa1b",
+    "url": "favicons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "4f5b58ae7d03847f1cd2",
+    "url": "app.js"
+  },
+  {
+    "revision": "4f5b58ae7d03847f1cd2",
+    "url": "app.css"
+  }
+];
